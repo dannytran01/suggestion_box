7ee8cf1c2ca2b808db518bd607656171e66715bc
Danny Tran <dannytran01@gmail.com>
2017-11-04 17:45:46 -0700

    Route set up in module along with creation of all needed controllers

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-var app = angular.module('SuggestionBox', []);
+var app = angular.module('SuggestionBox', ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/suggestions', {
+            controller: 'HomeController',
+            templateUrl: 'views/home.html'
+        })
+        .when('/add-suggestion', {
+            controller: 'SuggestionController',
+            templateUrl: 'views/add_suggestion.html'
+        })
+        .when('/suggestion/:suggestion-id/comment', {
+            controller: 'CommentController',
+            templateUrl: 'views/comment.html'
+        })
+        .otherwise({
+            redirectTo: '/suggestions'
+        })
+
+});
