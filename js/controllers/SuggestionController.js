2e75c8b24030a144fb50eab8ac5808f335e18424
Danny Tran <dannytran01@gmail.com>
2017-11-05 16:17:55 -0800

    Added ability to add new suggestion and append to the list

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -1,3 +1,9 @@
-app.controller('SuggestionController', ['$scope', function(){
+app.controller('SuggestionController', ['$scope', 'demoSuggestions', '$location', '$routeParams', function($scope, demoSuggestions, $location, $routeParams){
+
+    $scope.addNewSuggestion = function(){
+        var newSuggestion = {title:$scope.newSuggestion, upvotes:0, comments:[]};
+        demoSuggestions.posts.push(newSuggestion);
+        $location.path('/suggestions');
+    };
 
 }]);
